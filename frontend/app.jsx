30bf56b1692213b97cdbb5434e2cc7b8f606f509
brandonfang <bdmfang@gmail.com>
2021-06-01 11:13:24 -0700

    Set up static pages

---
+++ frontend/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Hello world</h1>, root);
+});

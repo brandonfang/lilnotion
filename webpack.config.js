ad36f85f9856af09e08478b71a8cefed936db0bc
brandonfang <bdmfang@gmail.com>
2021-06-01 10:58:18 -0700

    Create rails app

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env', '@babel/react'],
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*'],
+  },
+};

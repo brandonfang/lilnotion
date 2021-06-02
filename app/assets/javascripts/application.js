8a1a2000da88082a8684a436ba77b0e4a6cc1279
brandonfang <bdmfang@gmail.com>
2021-06-02 03:38:19 -0700

    Use ruby 2.5.1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require rails-ujs
+//= require activestorage
 //= require_tree .
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,6 +6,9 @@ class App extends React.Component {
       <div className="app">
         <h1>All-in-one workspace</h1>
         <h2>One tool for your whole team. Write, plan, and get organized.</h2>
+        <p>
+          <a href="#">Log in</a> or <a href="#">Sign up</a>
+        </p>
       </div>
     );
   }

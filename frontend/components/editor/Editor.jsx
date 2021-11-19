d16f4a5800bc0c206523d83aaa4b7d3f43a65d99
brandonfang <bdmfang@gmail.com>
2021-11-19 13:00:00 -0800

    Update editor component

--- frontend/components/editor/Editor.jsx
+++ frontend/components/editor/Editor.jsx
@@ -7,23 +7,40 @@ class Editor extends React.Component {
     super(props);
     this.state = {
       pageId: this.props.location.pathname.slice(3),
+      pages: {},
+      blocks: {},
+      isLoading: true,
     };
   }
 
-  componentDidMount() {
-    this.props.fetchPages(this.props.currentUser.id).then((res) => {
-      if (!res.pages[this.state.pageId]) {
-        const page = Object.values(res.pages)[0];
-        this.props.history.push(`/p/${page.id}`);
+  async componentDidMount() {
+    const pageRes = await this.props.fetchPages(this.props.currentUser.id);
+    const blockRes = await this.props.fetchBlocks(this.props.currentUser.id);
+    const pages = pageRes.pages;
+    const blocks = blockRes.blocks;
+    this.setState({
+      pages: pages,
+      blocks: blocks,
+      isLoading: false,
+    }, () => {
+      if (!pages[this.state.pageId]) {
+        const firstPage = Object.values(pages)[0];
+        this.props.history.push(`/p/${firstPage.id}`);
       }
-    });
+    })
   }
 
   render() {
+    if (this.state.isLoading) {
+      return (
+        <div>Loading...</div>
+        );
+    }
+
     return (
-      <div className="editor">
-        <SidebarContainer />
-        <PageContainer />
+      <div id="editor">
+        <SidebarContainer pages={this.state.pages} />
+        <PageContainer pages={this.state.pages} blocks={this.state.blocks} />
       </div>
     );
   }
--- frontend/components/editor/EditorContainer.js
+++ frontend/components/editor/EditorContainer.js
@@ -2,15 +2,18 @@ import { connect } from 'react-redux';
 import Editor from './Editor';
 import { logout } from '../../actions/session-actions';
 import { fetchPages } from '../../actions/page-actions';
+import { fetchBlocks } from '../../actions/block-actions';
 
 const mapStateToProps = (state, ownProps) => ({
   errors: state.errors.session,
   currentUser: state.entities.users[state.session.currentUserId],
   pages: state.entities.pages,
+  blocks: state.entities.blocks,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   fetchPages: (userId) => dispatch(fetchPages(userId)),
+  fetchBlocks: (userId) => dispatch(fetchBlocks(userId)),
   logout: () => dispatch(logout()),
 });
 

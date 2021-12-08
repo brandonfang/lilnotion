6a7dff43db8ea0c4b7daaf0ae6c1d15ad97ae943
brandonfang <bdmfang@gmail.com>
2021-12-07 19:40:18 -0800

    Clean up window

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from "react-dom";
 import Root from './components/Root';
 import configureStore from './store/store';
-import { signup, login, logout } from './actions/session-actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -25,12 +24,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
   // Only for debugging purposes; remove for production
-  window.currentUser = store.currentUser;
-  window.getState = store.getState
-  window.dispatch = store.dispatch
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
+  // window.currentUser = store.currentUser;
+  // window.getState = store.getState
+  // window.dispatch = store.dispatch
+  // window.signup = signup;
+  // window.login = login;
+  // window.logout = logout;
   
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);

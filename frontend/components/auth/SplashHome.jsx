05757b6cf89433b6418f4b13572f5aa413c17316
brandonfang <bdmfang@gmail.com>
2021-12-08 11:36:29 -0800

    Update persona grid

--- frontend/components/auth/SplashHome.jsx
+++ frontend/components/auth/SplashHome.jsx
@@ -15,35 +15,42 @@ const SplashHome = ({ login }) => {
   return (
     <>
       <NavBarContainer />
-      <main className="splash-main">
-        <div className="align-center-wrapper">
-          <div className="hero-image-wrapper">
-            <img src={window.hero} alt="People using Notion" />
-          </div>
-
-          <div className="center-text-wrapper">
-            <h1 className="hero-title">All-in-one workspace</h1>
-            <h2 className="hero-subtitle">
-              One tool for your whole team. Write, plan, and get organized.
-            </h2>
-
-            <div className="cta-container">
-              {/* <Link to="/login" className="cta cta-login">
-                Log in
-              </Link> */}
-              <div onClick={loginDemo} className="cta cta-try">
-                Try lilNotion now
+      <div className="splash-bg">
+        <main className="splash-main">
+          <div className="align-center-wrapper">
+            <div className="hero-image-wrapper">
+              <img src={window.hero} alt="People using Notion" />
+            </div>
+            <div className="center-text-wrapper">
+              <h1 className="hero-title">All-in-one workspace</h1>
+              <h2 className="hero-subtitle">
+                One tool for your whole team. Write, plan, and get organized.
+              </h2>
+              <div className="cta-container">
+                {/* <Link to="/login" className="cta cta-login">
+                  Log in
+                </Link> */}
+                <div onClick={loginDemo} className="cta cta-try">
+                  Try lilNotion now
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        
-        <div className="align-center-wrapper"> {/* margin vertical 80px */}
-          <h2>Build to empower every team</h2>
-          <h3>lilNotion solves problems unique to every function.</h3>
-          <div className="persona-grid"> {/* margin vertical 40px */}
+          <div className="align-center-wrapper">
+          </div>
+          <div className="align-center-wrapper showcase">
+            <div className="screenshot-wrapper">
+              <img classname="screenshot" src={window.wiki} alt="wiki" />
+            </div>
+          </div>
+          <div className="align-center-wrapper margin-l">
+            <h2 className="global-title">Build to empower every team</h2>
+            <h3 className="global-subtitle">lilNotion solves problems unique to every function.</h3>
+            <div className="persona-grid">
               <div className="persona-grid-item">
-                <div className="persona-grid-image"><img src={window.product} alt="product" /></div>
+                <div className="persona-grid-image">
+                  <img src={window.product} alt="product" />
+                </div>
                 <h4 className="persona-title">Product</h4>
                 <ul className="persona-list">
                   <li>Visualize your product roadmap</li>
@@ -52,7 +59,9 @@ const SplashHome = ({ login }) => {
                 </ul>
               </div>
               <div className="persona-grid-item">
-                <div className="persona-grid-image"><img src={window.engineering} alt="engineering" /></div>
+                <div className="persona-grid-image">
+                  <img src={window.engineering} alt="engineering" />
+                </div>
                 <h4 className="persona-title">Engineering</h4>
                 <ul className="persona-list">
                   <li>Coordinate releases</li>
@@ -61,7 +70,9 @@ const SplashHome = ({ login }) => {
                 </ul>
               </div>
               <div className="persona-grid-item">
-                <div className="persona-grid-image"><img src={window.hr} alt="hr" /></div>
+                <div className="persona-grid-image">
+                  <img src={window.hr} alt="hr" />
+                </div>
                 <h4 className="persona-title">HR</h4>
                 <ul className="persona-list">
                   <li>Create a company wiki</li>
@@ -70,7 +81,9 @@ const SplashHome = ({ login }) => {
                 </ul>
               </div>
               <div className="persona-grid-item">
-                <div className="persona-grid-image"><img src={window.design} alt="design" /></div>
+                <div className="persona-grid-image">
+                  <img src={window.design} alt="design" />
+                </div>
                 <h4 className="persona-title">Design</h4>
                 <ul className="persona-list">
                   <li>Track every project</li>
@@ -79,7 +92,9 @@ const SplashHome = ({ login }) => {
                 </ul>
               </div>
               <div className="persona-grid-item">
-                <div className="persona-grid-image"><img src={window.sales} alt="sales" /></div>
+                <div className="persona-grid-image">
+                  <img src={window.sales} alt="sales" />
+                </div>
                 <h4 className="persona-title">Sales</h4>
                 <ul className="persona-list">
                   <li>Build a flexible CRM</li>
@@ -88,7 +103,9 @@ const SplashHome = ({ login }) => {
                 </ul>
               </div>
               <div className="persona-grid-item">
-                <div className="persona-grid-image"><img src={window.marketing} alt="marketing" /></div>
+                <div className="persona-grid-image">
+                  <img src={window.marketing} alt="marketing" />
+                </div>
                 <h4 className="persona-title">Marketing</h4>
                 <ul className="persona-list">
                   <li>Make a style guide</li>
@@ -96,9 +113,10 @@ const SplashHome = ({ login }) => {
                   <li>Keep tabs on everything</li>
                 </ul>
               </div>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </>
   );
 };
